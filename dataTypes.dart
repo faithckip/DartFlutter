9aee285b172fc642642681f0f8fc9f4ba4ece539
Faith Kiprono <78324506+faithckip@users.noreply.github.com>
2024-02-26 21:50:45 +0100

    Add files via upload

---
+++ dataTypes.dart
@@ -0,0 +1,26 @@
+void main() {
+  // Integer
+  int integerNumber = 10; // Declaring an integer variable
+  print("Integer Number: $integerNumber");
+
+  // Double
+  double doubleNumber = 3.14; // Declaring a double variable
+  print("Double Number: $doubleNumber");
+
+  // String
+  String message = "Hello, Dart!"; // Declaring a string variable
+  print("Message: $message");
+
+  // List
+  List<int> numbers = [1, 2, 3, 4, 5]; // Declaring a list of integers
+  print("List of Numbers: $numbers");
+
+  // Map
+  Map<String, dynamic> person = {
+    // Declaring a map representing a person
+    'name': 'John',
+    'age': 30,
+    'isStudent': false,
+  };
+  print("Person Details: $person");
+}
